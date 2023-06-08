c6867e9ae11879668870db7befbcb5c174e1d811
arvnabil <nabilalcha58@gmail.com>
2023-06-08 15:38:16 +0700

    feat: init commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,649 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PagesController extends Controller
+{
+    public function elementsAvatar()
+    {
+        return view('pages.template.elements-avatar');
+    }
+
+    public function elementsAlert()
+    {
+        return view('pages.template.elements-alert');
+    }
+
+    public function elementsBadge()
+    {
+        return view('pages.template.elements-badge');
+    }
+
+    public function elementsBreadcrumb()
+    {
+        return view('pages.template.elements-breadcrumb');
+    }
+
+    public function elementsButton()
+    {
+        return view('pages.template.elements-button');
+    }
+
+    public function elementsButtonGroup()
+    {
+        return view('pages.template.elements-button-group');
+    }
+
+    public function elementsCard()
+    {
+        return view('pages.template.elements-card');
+    }
+
+    public function elementsDivider()
+    {
+        return view('pages.template.elements-divider');
+    }
+
+    public function elementsMask()
+    {
+        return view('pages.template.elements-mask');
+    }
+
+    public function elementsProgress()
+    {
+        return view('pages.template.elements-progress');
+    }
+
+    public function elementsSkeleton()
+    {
+        return view('pages.template.elements-skeleton');
+    }
+
+    public function elementsSpinner()
+    {
+        return view('pages.template.elements-spinner');
+    }
+
+    public function elementsTag()
+    {
+        return view('pages.template.elements-tag');
+    }
+
+    public function elementsTooltip()
+    {
+        return view('pages.template.elements-tooltip');
+    }
+
+    public function elementsTypography()
+    {
+        return view('pages.template.elements-typography');
+    }
+
+    public function componentsAccordion()
+    {
+        return view('pages.template.components-accordion');
+    }
+
+    public function componentsCollapse()
+    {
+        return view('pages.template.components-collapse');
+    }
+
+    public function componentsTab()
+    {
+        return view('pages.template.components-tab');
+    }
+
+    public function componentsDropdown()
+    {
+        return view('pages.template.components-dropdown');
+    }
+
+    public function componentsPopover()
+    {
+        return view('pages.template.components-popover');
+    }
+
+    public function componentsModal()
+    {
+        return view('pages.template.components-modal');
+    }
+
+    public function componentsDrawer()
+    {
+        return view('pages.template.components-drawer');
+    }
+
+    public function componentsSteps()
+    {
+        return view('pages.template.components-steps');
+    }
+
+    public function componentsTimeline()
+    {
+        return view('pages.template.components-timeline');
+    }
+
+    public function componentsPagination()
+    {
+        return view('pages.template.components-pagination');
+    }
+
+    public function componentsMenuList()
+    {
+        return view('pages.template.components-menu-list');
+    }
+
+    public function componentsTreeview()
+    {
+        return view('pages.template.components-treeview');
+    }
+
+    public function componentsTable()
+    {
+        return view('pages.template.components-table');
+    }
+
+    public function componentsTableAdvanced()
+    {
+        return view('pages.template.components-table-advanced');
+    }
+
+    public function componentsTableGridjs()
+    {
+        return view('pages.template.components-table-gridjs');
+    }
+
+    public function componentsApexchart()
+    {
+        return view('pages.template.components-apexchart');
+    }
+
+    public function componentsCarousel()
+    {
+        return view('pages.template.components-carousel');
+    }
+
+    public function componentsNotification()
+    {
+        return view('pages.template.components-notification');
+    }
+
+    public function componentsExtensionClipboard()
+    {
+        return view('pages.template.components-extension-clipboard');
+    }
+
+    public function componentsExtensionPersist()
+    {
+        return view('pages.template.components-extension-persist');
+    }
+
+    public function componentsExtensionMonochrome()
+    {
+        return view('pages.template.components-extension-monochrome');
+    }
+
+    public function formsLayoutV1()
+    {
+        return view('pages.template.forms-layout-v1');
+    }
+
+    public function formsLayoutV2()
+    {
+        return view('pages.template.forms-layout-v2');
+    }
+
+    public function formsLayoutV3()
+    {
+        return view('pages.template.forms-layout-v3');
+    }
+
+    public function formsLayoutV4()
+    {
+        return view('pages.template.forms-layout-v4');
+    }
+
+    public function formsLayoutV5()
+    {
+        return view('pages.template.forms-layout-v5');
+    }
+
+    public function formsInputText()
+    {
+        return view('pages.template.forms-input-text');
+    }
+
+    public function formsInputGroup()
+    {
+        return view('pages.template.forms-input-group');
+    }
+
+    public function formsInputMask()
+    {
+        return view('pages.template.forms-input-mask');
+    }
+
+    public function formsCheckbox()
+    {
+        return view('pages.template.forms-checkbox');
+    }
+
+    public function formsRadio()
+    {
+        return view('pages.template.forms-radio');
+    }
+
+    public function formsSwitch()
+    {
+        return view('pages.template.forms-switch');
+    }
+
+    public function formsSelect()
+    {
+        return view('pages.template.forms-select');
+    }
+
+    public function formsTomSelect()
+    {
+        return view('pages.template.forms-tom-select');
+    }
+
+    public function formsTextarea()
+    {
+        return view('pages.template.forms-textarea');
+    }
+
+    public function formsRange()
+    {
+        return view('pages.template.forms-range');
+    }
+
+    public function formsDatepicker()
+    {
+        return view('pages.template.forms-datepicker');
+    }
+
+    public function formsTimepicker()
+    {
+        return view('pages.template.forms-timepicker');
+    }
+
+    public function formsDatetimepicker()
+    {
+        return view('pages.template.forms-datetimepicker');
+    }
+
+    public function formsTextEditor()
+    {
+        return view('pages.template.forms-text-editor');
+    }
+
+    public function formsUpload()
+    {
+        return view('pages.template.forms-upload');
+    }
+
+    public function formsValidation()
+    {
+        return view('pages.template.forms-validation');
+    }
+
+    public function layoutsOnboarding1()
+    {
+        return view('pages.template.layouts-onboarding-1');
+    }
+
+    public function layoutsOnboarding2()
+    {
+        return view('pages.template.layouts-onboarding-2');
+    }
+
+    public function layoutsUserCard1()
+    {
+        return view('pages.template.layouts-user-card-1');
+    }
+
+    public function layoutsUserCard2()
+    {
+        return view('pages.template.layouts-user-card-2');
+    }
+
+    public function layoutsUserCard3()
+    {
+        return view('pages.template.layouts-user-card-3');
+    }
+
+    public function layoutsUserCard4()
+    {
+        return view('pages.template.layouts-user-card-4');
+    }
+
+    public function layoutsUserCard5()
+    {
+        return view('pages.template.layouts-user-card-5');
+    }
+
+    public function layoutsUserCard6()
+    {
+        return view('pages.template.layouts-user-card-6');
+    }
+
+    public function layoutsUserCard7()
+    {
+        return view('pages.template.layouts-user-card-7');
+    }
+
+    public function layoutsBlogCard1()
+    {
+        return view('pages.template.layouts-blog-card-1');
+    }
+
+    public function layoutsBlogCard2()
+    {
+        return view('pages.template.layouts-blog-card-2');
+    }
+
+    public function layoutsBlogCard3()
+    {
+        return view('pages.template.layouts-blog-card-3');
+    }
+
+    public function layoutsBlogCard4()
+    {
+        return view('pages.template.layouts-blog-card-4');
+    }
+
+    public function layoutsBlogCard5()
+    {
+        return view('pages.template.layouts-blog-card-5');
+    }
+
+    public function layoutsBlogCard6()
+    {
+        return view('pages.template.layouts-blog-card-6');
+    }
+
+    public function layoutsBlogCard7()
+    {
+        return view('pages.template.layouts-blog-card-7');
+    }
+
+    public function layoutsBlogCard8()
+    {
+        return view('pages.template.layouts-blog-card-8');
+    }
+
+    public function layoutsBlogDetails()
+    {
+        return view('pages.template.layouts-blog-details');
+    }
+
+    public function layoutsHelp1()
+    {
+        return view('pages.template.layouts-help-1');
+    }
+
+    public function layoutsHelp2()
+    {
+        return view('pages.template.layouts-help-2');
+    }
+
+    public function layoutsHelp3()
+    {
+        return view('pages.template.layouts-help-3');
+    }
+
+    public function layoutsPriceList1()
+    {
+        return view('pages.template.layouts-price-list-1');
+    }
+
+    public function layoutsPriceList2()
+    {
+        return view('pages.template.layouts-price-list-2');
+    }
+
+    public function layoutsPriceList3()
+    {
+        return view('pages.template.layouts-price-list-3');
+    }
+
+    public function layoutsInvoice1()
+    {
+        return view('pages.template.layouts-invoice-1');
+    }
+
+    public function layoutsInvoice2()
+    {
+        return view('pages.template.layouts-invoice-2');
+    }
+
+    public function layoutsSignIn1()
+    {
+        return view('pages.template.layouts-sign-in-1');
+    }
+
+    public function layoutsSignIn2()
+    {
+        return view('pages.template.layouts-sign-in-2');
+    }
+
+    public function layoutsSignUp1()
+    {
+        return view('pages.template.layouts-sign-up-1');
+    }
+
+    public function layoutsSignUp2()
+    {
+        return view('pages.template.layouts-sign-up-2');
+    }
+
+    public function layoutsError4041()
+    {
+        return view('pages.template.layouts-error-404-1');
+    }
+
+    public function layoutsError4042()
+    {
+        return view('pages.template.layouts-error-404-2');
+    }
+
+    public function layoutsError4043()
+    {
+        return view('pages.template.layouts-error-404-3');
+    }
+
+    public function layoutsError4044()
+    {
+        return view('pages.template.layouts-error-404-4');
+    }
+
+    public function layoutsError401()
+    {
+        return view('pages.template.layouts-error-401');
+    }
+
+    public function layoutsError429()
+    {
+        return view('pages.template.layouts-error-429');
+    }
+
+    public function layoutsError500()
+    {
+        return view('pages.template.layouts-error-500');
+    }
+
+    public function layoutsStarterBlurredHeader()
+    {
+        return view('pages.template.layouts-starter-blurred-header');
+    }
+
+    public function layoutsStarterUnblurredHeader()
+    {
+        return view('pages.template.layouts-starter-unblurred-header');
+    }
+
+    public function layoutsStarterCenteredLink()
+    {
+        return view('pages.template.layouts-starter-centered-link');
+    }
+
+    public function layoutsStarterMinimalSidebar()
+    {
+        return view('pages.template.layouts-starter-minimal-sidebar');
+    }
+
+    public function layoutsStarterSideblock()
+    {
+        return view('pages.template.layouts-starter-sideblock');
+    }
+
+    public function appsChat()
+    {
+        return view('pages.template.apps-chat');
+    }
+
+    public function appsFilemanager()
+    {
+        return view('pages.template.apps-filemanager');
+    }
+
+    public function appsKanban()
+    {
+        return view('pages.template.apps-kanban');
+    }
+
+    public function appsList()
+    {
+        return view('pages.template.apps-list');
+    }
+
+    public function appsMail()
+    {
+        return view('pages.template.apps-mail');
+    }
+
+    public function appsNft1()
+    {
+        return view('pages.template.apps-nft-1');
+    }
+    public function appsNft2()
+    {
+        return view('pages.template.apps-nft-2');
+    }
+
+    public function appsPos()
+    {
+        return view('pages.template.apps-pos');
+    }
+
+    public function appsTodo()
+    {
+        return view('pages.template.apps-todo');
+    }
+
+    public function appsTravel()
+    {
+        return view('pages.template.apps-travel');
+    }
+
+    public function dashboardsCrmAnalytics()
+    {
+        return view('pages.template.dashboards-crm-analytics');
+    }
+
+    public function dashboardsOrders()
+    {
+        return view('pages.template.dashboards-orders');
+    }
+
+    public function dashboardsCrypto1()
+    {
+        return view('pages.template.dashboards-crypto1');
+    }
+
+    public function dashboardsCrypto2()
+    {
+        return view('pages.template.dashboards-crypto2');
+    }
+
+    public function dashboardsBanking1()
+    {
+        return view('pages.template.dashboards-banking1');
+    }
+
+    public function dashboardsBanking2()
+    {
+        return view('pages.template.dashboards-banking2');
+    }
+
+    public function dashboardsPersonal()
+    {
+        return view('pages.template.dashboards-personal');
+    }
+
+    public function dashboardsCmsAnalytics()
+    {
+        return view('pages.template.dashboards-cms-analytics');
+    }
+
+    public function dashboardsInfluencer()
+    {
+        return view('pages.template.dashboards-influencer');
+    }
+
+    public function dashboardsTravel()
+    {
+        return view('pages.template.dashboards-travel');
+    }
+
+    public function dashboardsTeacher()
+    {
+        return view('pages.template.dashboards-teacher');
+    }
+
+    public function dashboardsAuthors()
+    {
+        return view('pages.template.dashboards-authors');
+    }
+
+    public function dashboardsEducation()
+    {
+        return view('pages.template.dashboards-education');
+    }
+    public function dashboardsDoctor()
+    {
+        return view('pages.template.dashboards-doctor');
+    }
+
+    public function dashboardsEmployees()
+    {
+        return view('pages.template.dashboards-employees');
+    }
+
+    public function dashboardsWorkspaces()
+    {
+        return view('pages.template.dashboards-workspaces');
+    }
+
+    public function dashboardsMeetings()
+    {
+        return view('pages.template.dashboards-meetings');
+    }
+
+    public function dashboardsProjectBoards()
+    {
+        return view('pages.template.dashboards-project-boards');
+    }
+
+    public function dashboardsWidgetUi()
+    {
+        return view('pages.template.dashboards-widget-ui');
+    }
+
+    public function dashboardsWidgetContacts()
+    {
+        return view('pages.template.dashboards-widget-contacts');
+    }
+}
